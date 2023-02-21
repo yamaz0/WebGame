bde2b0f770664d837a1848abdb2b0329825877cc
yamaz0 <szulc.patryk1c@gmail.com>
2023-02-21 18:03:34 +0100

    Improved DbGameContext

--- WebGame.Persistence.EF/DbGameContext.cs
+++ WebGame.Persistence.EF/DbGameContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Reflection.Emit;
+using WebGame.Application.Constants;
+using WebGame.Domain.Common;
 using WebGame.Domain.Entities.Player;
 using WebGame.Domain.Entities.User;
 using WebGame.Entities;
@@ -22,8 +24,36 @@ namespace WebGame
         public DbSet<Mission> Missions { get; set; }
 
         public DbGameContext(DbContextOptions options) : base(options) { }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.ApplyConfigurationsFromAssembly(typeof(DbGameContext).Assembly);
+
             var converter = new EnumToStringConverter<ItemType>();
 
             builder.Entity<BodyArmor>()
@@ -33,13 +63,13 @@ namespace WebGame
             builder.Entity<Player>(eb =>
             {
                 eb.Property(x => x.Name).IsRequired();
-                eb.Property(x => x.Exp).HasDefaultValue(0);
-                eb.Property(x => x.Level).HasDefaultValue(1);
-                eb.Property(x => x.SkillPoints).HasDefaultValue(0);
-                eb.Property(x => x.Cash).HasDefaultValue(0);
-                eb.Property(x => x.Strenght).HasDefaultValue(10);
-                eb.Property(x => x.Dexterity).HasDefaultValue(10);
-                eb.Property(x => x.Endurance).HasDefaultValue(10);
+                eb.Property(x => x.Exp).HasDefaultValue(ConstantsPlayer.DEFAULT_EXP);
+                eb.Property(x => x.Level).HasDefaultValue(ConstantsPlayer.DEFAULT_LEVEL);
+                eb.Property(x => x.SkillPoints).HasDefaultValue(ConstantsPlayer.DEFAULT_SKILL_POINTS);
+                eb.Property(x => x.Cash).HasDefaultValue(ConstantsPlayer.DEFAULT_CASH);
+                eb.Property(x => x.Strenght).HasDefaultValue(ConstantsPlayer.DEFAULT_STRENGHT);
+                eb.Property(x => x.Dexterity).HasDefaultValue(ConstantsPlayer.DEFAULT_DEXTERITY);
+                eb.Property(x => x.Endurance).HasDefaultValue(ConstantsPlayer.DEFAULT_ENDURANCE);
                 eb.Property(x => x.JobId).HasDefaultValue(0);
             });
 
