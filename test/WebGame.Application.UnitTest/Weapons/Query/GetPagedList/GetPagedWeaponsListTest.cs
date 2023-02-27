b7a9dfa3d4b1aae3e13c72e3f6db5b05fa0d2194
yamaz0 <szulc.patryk1c@gmail.com>
2023-02-27 11:39:31 +0100

    Added GetPagedWeaponsList Test

---
+++ test/WebGame.Application.UnitTest/Weapons/Query/GetPagedList/GetPagedWeaponsListTest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebGame.Application.Automapper;
+using WebGame.Application.Functions.Weapons.Query.GetListWeapons;
+using WebGame.Application.Interfaces.Persistence;
+using WebGame.Application.UnitTest.Mocks.Repository;
+
+namespace WebGame.Application.UnitTest.Weapons.Query.GetPagedList
+{
+    public class GetPagedWeaponsListTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IWeaponRepository> _mockWeaponRepository;
+
+        public GetPagedWeaponsListTest()
+        {
+            _mockWeaponRepository = WeaponRepositoryMocks.GetWeaponRepository();
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+            _mapper = new Mapper(config);
+        }
+
+        [Fact]
+        public async void GetPagedWeaponsList()
+        {
+            var handler = new GetListWeaponsRequestHandler(_mockWeaponRepository.Object, _mapper);
+            var result = await handler.Handle(new GetListWeaponsRequest(), CancellationToken.None);
+            result.ShouldBeOfType(typeof(List<GetListWeaponsViewModel>));
+        }
+    }
+}
