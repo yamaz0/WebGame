03471ec8bbedb14671a297d8585daf5e9ac4d184
yamaz0 <szulc.patryk1c@gmail.com>
2023-04-22 22:39:33 +0200

    docker stuff

--- WebGame.UI.Blazor/Program.cs
+++ WebGame.UI.Blazor/Program.cs
@@ -11,13 +11,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://dasdsdas-szulcpatryk1c.b4a.run") });
 
 builder.Services.AddScoped<IClient, Client>();
 builder.Services.AddScoped<AuthorizeDelegateHanlder>();
 builder.Services.ConfigureServices();
 
-builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:5001"));
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://dasdsdas-szulcpatryk1c.b4a.run"));
     //.AddHttpMessageHandler<AuthorizeDelegateHanlder>();
 
 await builder.Build().RunAsync();
