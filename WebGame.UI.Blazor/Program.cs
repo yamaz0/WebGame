b369823dbdd27fdb4ac54fd87f9d1f105228a334
yamaz0 <szulc.patryk1c@gmail.com>
2023-04-30 23:04:23 +0200

    Update Program.cs

--- WebGame.UI.Blazor/Program.cs
+++ WebGame.UI.Blazor/Program.cs
@@ -15,13 +15,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(url) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://dasdsdas-szulcpatryk1c.b4a.run") });
 
 builder.Services.AddScoped<IClient, Client>();
 builder.Services.AddScoped<AuthorizeDelegateHanlder>();
 builder.Services.ConfigureServices();
 
-builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri(url));
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://dasdsdas-szulcpatryk1c.b4a.run"));
     //.AddHttpMessageHandler<AuthorizeDelegateHanlder>();
 
 await builder.Build().RunAsync();
